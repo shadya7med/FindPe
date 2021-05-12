8eb955dfb28465dc29954c2709d6019de84a57e7
SaidElsoudany <62873010+SaidElsoudany@users.noreply.github.com>
2021-05-12 20:43:26 +0200

    layouts

--- app/src/main/java/com/iti/example/findpe/Home/Traveling/Views/TravelingFragment.kt
+++ app/src/main/java/com/iti/example/findpe/Home/Traveling/Views/TravelingFragment.kt
@@ -6,12 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.iti.example.findpe.R
+import com.iti.example.findpe.databinding.FragmentTripDetailsBinding
 
 
 class TravelingFragment : Fragment() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -20,7 +21,8 @@ class TravelingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_traveling, container, false)
+        return inflater.inflate(R.layout.fragment_trip_details, container, false)
+
     }
 
 
