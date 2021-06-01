250360bb2ec94cfd34cd8703164cd9a6822403f4
SaidElsoudany <62873010+SaidElsoudany@users.noreply.github.com>
2021-06-01 20:03:40 +0200

    ui update
    progress bar while loading
    error msg in case of error msg
    dark mode

--- app/src/main/java/com/iti/example/findpe2/tripCheckout/tripDetails/Views/TripDetailsFragment.kt
+++ app/src/main/java/com/iti/example/findpe2/tripCheckout/tripDetails/Views/TripDetailsFragment.kt
@@ -1,13 +1,14 @@
 package com.iti.example.findpe2.tripCheckout.tripDetails.views
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import com.iti.example.findpe2.R
 import com.iti.example.findpe2.databinding.FragmentTripDetailsBinding
 import com.iti.example.findpe2.pojos.Trip
 
@@ -16,14 +17,11 @@ class TripDetailsFragment:Fragment() {
 
 
     lateinit var binding:FragmentTripDetailsBinding
+    lateinit var viewModel: TripDetailsViewModel
     private lateinit var navController: NavController
 
     //it should be obtained from the Intent that open this trip details
     private var tripId = 0
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,12 +34,20 @@ class TripDetailsFragment:Fragment() {
 
         val viewModelFactory = TripDetailsViewModelFactory((arguments?.get("trip") as Trip))
 
-        val viewModel = ViewModelProvider(this, viewModelFactory).get(TripDetailsViewModel::class.java)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(TripDetailsViewModel::class.java)
 
         binding.viewModel = viewModel
 
         binding.lifecycleOwner = this
 
+        viewModel.navigateToBooking.observe(viewLifecycleOwner, Observer { tripId ->
+            tripId?.let {
+                navController.navigate(TripDetailsFragmentDirections.actionTripDetailsFragmentToBookingFragment(tripId))
+                viewModel.displayBookingComplete()
+            }
+        })
+
+        setHasOptionsMenu(true)
         return binding.root
     }
 
@@ -50,7 +56,25 @@ class TripDetailsFragment:Fragment() {
         navController = view.findNavController()
     }
 
-    fun navigateToTripBooking(){
-        navController.navigate(TripDetailsFragmentDirections.actionTripDetailsFragmentToBookingFragment(tripId))
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.trip_details_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home -> {
+                activity?.finish()
+                true
+            }
+            R.id.book_trip -> {
+                viewModel.displayBooking(tripId)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+
+        }
     }
 }
--- app/src/main/java/com/iti/example/findpe2/tripCheckout/tripDetails/Views/TripDetailsViewModel.kt
+++ app/src/main/java/com/iti/example/findpe2/tripCheckout/tripDetails/Views/TripDetailsViewModel.kt
@@ -8,11 +8,21 @@ import com.iti.example.findpe2.pojos.Trip
 class TripDetailsViewModel(trip: Trip): ViewModel() {
 
     private val _selectedTrip = MutableLiveData<Trip>()
-
     val selectedTrip: LiveData<Trip>
         get() = _selectedTrip
 
+    private val _navigateToBooking = MutableLiveData<Int?>()
+    val navigateToBooking: LiveData<Int?>
+        get() = _navigateToBooking
+
     init {
         _selectedTrip.value = trip
+    }
+
+    fun displayBooking(tripId: Int){
+        _navigateToBooking.value = tripId
+    }
+    fun displayBookingComplete(){
+        _navigateToBooking.value = null
     }
 }
