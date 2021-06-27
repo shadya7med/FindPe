06b63f183be93825f7ccfc5f53508920a163ca9d
SaidElsoudany <62873010+SaidElsoudany@users.noreply.github.com>
2021-06-27 08:36:22 +0200

    Update BindingAdapters.kt

--- app/src/main/java/com/iti/example/findpe2/utils/BindingAdapters.kt
+++ app/src/main/java/com/iti/example/findpe2/utils/BindingAdapters.kt
@@ -461,8 +461,8 @@ fun ImageView.imageUrlTimelineSlot(title: String?) {
     var imageUrl = ""
     if (title != null) {
         imageUrl = when(title){
-            "Breakfast" -> "https://simply-delicious-food.com/wp-content/uploads/2018/10/breakfast-board-500x500.jpg"
-            "Lunch" -> "https://img.taste.com.au/45Y4XB7k/w1200-h630-cfill/taste/2018/02/barbecued-chicken-rice-bowls-taste-135064-1.jpg"
+            "Breakfast at Hotel" -> "https://simply-delicious-food.com/wp-content/uploads/2018/10/breakfast-board-500x500.jpg"
+            "Lunch by the sea" -> "https://img.taste.com.au/45Y4XB7k/w1200-h630-cfill/taste/2018/02/barbecued-chicken-rice-bowls-taste-135064-1.jpg"
             else -> "https://assets.kraftfoods.com/recipe_images/opendeploy/62663_640x428.jpg"
         }
         Glide.with(this.context)
